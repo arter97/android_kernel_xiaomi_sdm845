18944daa6b07a01feb4edf3c36fa0e86b2864e7c
Park Ju Hyung <qkrwngud825@gmail.com>
2019-03-25 03:13:30 +0900

    build: merge build tools from my OnePlus 6 kernel
    Signed-off-by: Park Ju Hyung <qkrwngud825@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -3,6 +3,9 @@ export KERNELDIR=`readlink -f .`
 export RAMFS_SOURCE=`readlink -f $KERNELDIR/ramdisk`
 export PARTITION_SIZE=67108864
 
+export OS="9.0.0"
+export SPL="2019-02"
+
 echo "kerneldir = $KERNELDIR"
 echo "ramfs_source = $RAMFS_SOURCE"
 
@@ -11,12 +14,24 @@ RAMFS_TMP="/tmp/arter97-dipper-ramdisk"
 echo "ramfs_tmp = $RAMFS_TMP"
 cd $KERNELDIR
 
-if [ "${1}" = "skip" ] ; then
+stock=0
+if [[ "${1}" == "stock" ]] ; then
+	stock=1
+	shift
+fi
+
+if [[ "${1}" == "skip" ]] ; then
 	echo "Skipping Compilation"
 else
+	if [[ "$stock" == "1" ]] ; then
+		sed -i -e 's@"want_initramfs"@"skip_initramfs"@g' init/initramfs.c
+	fi
 	echo "Compiling kernel"
 	cp defconfig .config
 	make "$@" || exit 1
+	if [[ "$stock" == "1" ]] ; then
+		sed -i -e 's@"skip_initramfs"@"want_initramfs"@g' init/initramfs.c
+	fi
 fi
 
 echo "Building new ramdisk"
@@ -32,29 +47,38 @@ cd $RAMFS_TMP
 find . -name .git -exec rm -rf {} \;
 find . -name EMPTY_DIRECTORY -exec rm -rf {} \;
 
+sed -i -e s@ro.build.version.release.*@ro.build.version.release=${OS}@g \
+       -e s@ro.build.version.security_patch.*@ro.build.version.security_patch=${SPL}-01@g prop.default
+
+if [[ "$stock" == "1" ]] ; then
+	# Don't use Magisk
+	mv .backup/init init
+	rm -rf .backup
+fi
+
 $KERNELDIR/ramdisk_fix_permissions.sh 2>/dev/null
 
 cd $KERNELDIR
 rm -rf $RAMFS_TMP/tmp/*
 
 cd $RAMFS_TMP
-find . | fakeroot cpio -H newc -o | gzip -9 > $RAMFS_TMP.cpio.gz
-ls -lh $RAMFS_TMP.cpio.gz
+find . | fakeroot cpio -H newc -o | lz4 -l -9 > $RAMFS_TMP.cpio.lz4
+ls -lh $RAMFS_TMP.cpio.lz4
 cd $KERNELDIR
 
 echo "Making new boot image"
 mkbootimg \
     --kernel $KERNELDIR/arch/arm64/boot/Image.gz-dtb \
-    --ramdisk $RAMFS_TMP.cpio.gz \
-    --cmdline 'console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 buildvariant=user' \
+    --ramdisk $RAMFS_TMP.cpio.lz4 \
+    --cmdline 'console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA84000 androidboot.hardware=qcom androidboot.console=ttyMSM0 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 service_locator.enable=1 swiotlb=2048 androidboot.configfs=true firmware_class.path=/vendor/firmware_mnt/image loop.max_part=7 androidboot.usbcontroller=a600000.dwc3 buildvariant=user printk.devkmsg=on' \
     --base           0x00000000 \
     --pagesize       4096 \
     --kernel_offset  0x00008000 \
     --ramdisk_offset 0x01000000 \
     --second_offset  0x00f00000 \
     --tags_offset    0x00000100 \
-    --os_version     9.0.0 \
-    --os_patch_level 2019-02 \
+    --os_version     $OS \
+    --os_patch_level $SPL \
     --header_version 1 \
     -o $KERNELDIR/boot.img
 
